5ad1a1cead41eb40062c5cbd8eb498161b340d40
eanteby <eanteby2@gmail.com>
2024-08-04 16:17:20 -0400

    added planet

--- 1-table/1 table.sql
+++ 1-table/1 table.sql
@@ -4,6 +4,7 @@ drop table if exists dbo.body
 go
 create table dbo.body(
     BodyId int not null identity primary key,
+    Planet varchar (50) not null,
     bodypart varchar(100) not null, 
-	num int null
+    num int null
 )
