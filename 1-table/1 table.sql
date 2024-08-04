86b11a969630ccf61f572ce6a22ca69276ffe7d2
eanteby <eanteby2@gmail.com>
2024-08-04 16:08:21 -0400

    made num column nullable

--- 1-table/1 table.sql
+++ 1-table/1 table.sql
@@ -1,6 +1,9 @@
 use humanbodyDB
 go 
+drop table if exists dbo.body
+go
 create table dbo.body(
     BodyId int not null identity primary key,
-    bodypart varchar(100) not null, num int not null
+    bodypart varchar(100) not null, 
+	num int null
 )
